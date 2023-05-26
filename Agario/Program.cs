fc231129f637ac7b386a78da85485692d70e0e76
Serhii <serikgorbanev@gmail.com>
2023-05-26 19:04:22 +0200

    Started working. the same problem with window that was in 2D golf

---
+++ Agario/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Agario
+{
+    internal class Program
+    {
+        static public RenderWindow window;
+
+        static void Main(string[] args)
+        {
+            VideoMode videoMode = new VideoMode(500, 500);
+
+            window = new RenderWindow(videoMode, "2D golf");
+        }
+    }
+}
---
+++ Engine/StateMachine.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    class StateMachine
+    {
+        private long ticksBetweenFrames = 0;
+        public long lastTimingTick = 0;
+
+        State state;
+
+        public long neededFPS
+        {
+            get
+               => TimeSpan.TicksPerSecond / ticksBetweenFrames;
+            set
+               => ticksBetweenFrames = TimeSpan.TicksPerSecond / neededFPS;
+        }
+
+        public void SetState(State state)
+        {
+            this.state = state;
+        }
+
+        public void Update()
+        {
+            state.Update();
+        }
+
+        public void Render()
+        {
+            state.Render();
+        }
+
+        public void Input()
+        {
+            state.Input();
+        }
+        
+        public void Timing()
+        {
+            if (DateTime.Now.Ticks - lastTimingTick < TimeSpan.TicksPerSecond / neededFPS)
+            {
+                System.Threading.Thread.Sleep((int)(ticksBetweenFrames - DateTime.Now.Ticks - lastTimingTick));
+            }
+            lastTimingTick = DateTime.Now.Ticks;
+        }
+    }
+}
